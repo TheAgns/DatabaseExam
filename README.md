e4a5c350baf39ad42308ee5f363cde6b35d98f0f
Theagns <markus.agnsgaard@hotmail.com>
2023-05-26 19:41:11 +0200

    more

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Database exam using MSSQL, Neo4j & MongoDB
+## Table of contents
+- [Nunamarket using MSSQL, Neo4j & MongoDB](#Nunamarket-using-MSSQL,-Neo4j-&-MongoDB)
+- [Step 1](#step-1)
+  - [Folder structure:](#folder-structure)
+- [Step 2](#step-2)
+  - [Project solution:](#Project-resume)
+  - [Argumentation of Choices:](#argumentation-of-choices)
+- [Step 3](#step-3)
+  - [Database diagrams:](#Database-diagrams)
+  - [Further development/improvements](#further-developmentimprovements)
+
+# Step 1
+## Folder structure:
+📁data --> all data including csv and json files for NEO4j, MSSQL & MongoDB
+
+📁serverclient --> our NunaMarket application using next.js (Application code) readme on how to run in, inside the folder
+
+📁Neo4j --> scripts for Neo4j
+
+📁MSSQL --> scripts for Mssql
+
+📁MongoDB --> scripts for MongoDB
+
+# Step 2
+## 
+
--- serverclient/src/lib/sql.js
+++ serverclient/src/lib/sql.js
@@ -1,6 +1,5 @@
 const sql = require("mssql");
 
-<<<<<<< HEAD
 export async function getUser(userId) {
   try {
     await sql.connect(
@@ -15,61 +14,3 @@ export async function getUser(userId) {
 //typeof res;
 
 module.exports = { getUser };
-=======
-var Connection = require("tedious").Connection;
-var config = {
-  server: `127.0.0.1\\mssql`, //update me
-  authentication: {
-    type: "default",
-    options: {
-      userName: "sa", //update me
-      password: "Pass1234", //update me
-    },
-  },
-  options: {
-    // If you are on Microsoft Azure, you need encryption:
-    encrypt: true,
-    database: "mssql",
-    trustServerCertificate: true,
-    port: 50000
-  },
-};
-
-var Request = require("tedious").Request;
-var TYPES = require("tedious").TYPES;
-
-const getUsers = () => {
-  return new Promise((resolve, reject) => {
-    const connection = new Connection(config);
-    connection.on("connect", (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      console.log("connected");
-
-      const request = new Request(
-        "SELECT * FROM users",
-        (err, rowCount, rows) => {
-          if (err) {
-            console.log(err)
-            reject(err);
-          } else {
-            resolve(rows);
-          }
-
-          connection.close();
-        }
-      );
-
-      connection.execSql(request);
-    });
-
-    connection.connect();
-  });
-};
-
-export { getUsers };
-
-module.exports = { getUsers };
->>>>>>> 7e881755a4549c1c92df01c168ff4584bb98bdfa
